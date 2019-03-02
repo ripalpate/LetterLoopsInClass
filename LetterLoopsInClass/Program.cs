a839adbe061225da848da0d0ea3a21f8ee2ecbdd
Ripal Patel <ripal212007@gmail.com>
2019-03-02 11:01:38 -0600

    Done

--- LetterLoopsInClass/Program.cs
+++ LetterLoopsInClass/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
 
 namespace LetterLoopsInClass
 {
@@ -6,7 +8,24 @@ namespace LetterLoopsInClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter some Letters!");
+            var input = Console.ReadLine().ToLower();
+            var loopCount = 0;
+            //var myCharacters = new List<string>();
+
+            //foreach (var character in input)
+            //{
+            //    var letterString = character.ToString().ToUpper();
+            //        letterString +=  new string(character, loopCount);
+            //    myCharacters.Add(letterString);
+            //    loopCount++;
+            // where the varible being incremented. so if it is ++loopcount then it will display 2 and starts.
+            // }
+            //Console.WriteLine(string.Join('-', myCharacters));
+            var output = string.Join('-', input.ToCharArray().Select(x => x.ToString().ToUpper() + new string(x, loopCount++)));
+            //Select is method like map it does for loop and call anonymous function or linq
+            Console.WriteLine(output);
+            Console.ReadLine();
         }
     }
 }
